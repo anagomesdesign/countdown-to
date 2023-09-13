36a14b33ca6ab537951467b40a8930682aa3a92a
Ana Gomes <ana.siqueiragomes@gmail.com>
2023-09-13 10:43:19 -0500

    Update script.js
    Minor typo adjustment

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // The date we're counting down to with Brasilia timezone
-var countDownDate = new Date("Sep 03 2023 17:00:00 UTC-0300 (BRT)").getTime();
+var countDownDate = new Date("Sept 03 2023 17:00:00 UTC-0300 (BRT)").getTime();
 
 
 // Update the count down every 1 second
