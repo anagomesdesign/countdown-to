90dea8312e9947e30fb8eb789a4072d63902ad42
anagomesdesign <ana.siqueiragomes@gmail.com>
2025-03-18 16:30:50 -0500

    Add countdown timer component and update layout metadata

---
+++ src/components/CountdownTimer/CountdownTimer.tsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react';
+import './CountdownTimer.css';
+
+const CountdownTimer = () => {
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  
+    function calculateTimeLeft() {
+    // The date we're counting down to
+    const countDownDate = new Date("Apr 12 2025 19:00:00 GMT-0300 (BRT)").getTime();
+      
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+      
+      if (distance < 0) {
+        return { expired: true };
+      }
+  
+      return {
+        days: Math.floor(distance / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+        minutes: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+        seconds: Math.floor((distance % (1000 * 60)) / 1000),
+        expired: false,
+      };
+    }
+  
+    useEffect(() => {
+      const timer = setInterval(() => {
+        setTimeLeft(calculateTimeLeft());
+      }, 1000);
+  
+      return () => clearInterval(timer);
+    }, []);
+  
+    return (
+    
+    <div className="countdown">
+      {timeLeft.expired ? (
+        // If the countdown has expired, show this message:
+        <p>See you soon!</p>
+      ) : (
+        <p>
+        {timeLeft.days}d {timeLeft.hours}h {timeLeft.minutes}m {timeLeft.seconds}s
+        </p>
+      )}
+    </div>
+    );
+  };
+  
+  export default CountdownTimer;  
