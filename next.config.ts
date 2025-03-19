a69cc758234c2657669608d01554edee3531f6a0
anagomesdesign <ana.siqueiragomes@gmail.com>
2025-03-19 17:06:14 -0500

    Add video background feature and update asset handling

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(mp4|webm|ogg|mp3|wav|flac|aac|rtf|doc|docx)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: 'static/media/[name].[hash].[ext]',
+          publicPath: '/_next/',
+        },
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,25 @@
 
 import React from 'react';
 import CountdownTimer from '../components/CountdownTimer/CountdownTimer';
+import './page.css';
+
+
 
 export default function Home() {
+
+  const video = '/assets/bg-video.mp4';
+  
   return (
-   <CountdownTimer> 
-   </CountdownTimer>
+    <div>
+      <video
+        autoPlay
+        loop
+        muted
+      >
+        <source src={video} type="video/mp4" />
+      </video>
+      
+      <CountdownTimer/>    
+    </div>  
   );
 }
